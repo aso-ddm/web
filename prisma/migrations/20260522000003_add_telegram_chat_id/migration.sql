da3991c282f9fecda8eddde982df0f67857e4511
Alejandro Matutano <19matu91@gmail.com>
2026-05-23 01:29:02 +0200

    migration ok

--- prisma/migrations/20260522000003_add_telegram_chat_id/migration.sql
+++ prisma/migrations/20260522000003_add_telegram_chat_id/migration.sql
@@ -1,5 +1,7 @@
 -- AlterTable
+ALTER TABLE "Usuario" ADD COLUMN "alias_telegram" TEXT;
 ALTER TABLE "Usuario" ADD COLUMN "telegram_chat_id" BIGINT;
+ALTER TABLE "Usuario" ADD COLUMN "telegram_linked_at" TIMESTAMP(3);
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Usuario_telegram_chat_id_key" ON "Usuario"("telegram_chat_id");
